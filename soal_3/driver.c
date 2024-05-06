82602c4d39be1085a94294176b83c0a3cdebb61d
agnesgriselda <150202762+agnesgriselda@users.noreply.github.com>
2024-05-06 17:33:18 +0700

    Update driver.c

--- soal_3/driver.c
+++ soal_3/driver.c
@@ -6,10 +6,26 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <time.h> // Include time.h for time-related functions
 
 
 #define SERVER_IP "127.0.0.1"
 #define PORT 8000
+
+void log_message(char* command, char* info) {
+    FILE *fptr;
+    fptr = fopen("race.log", "a");
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    time_t now;
+    time(&now);
+    char timestamp[20];
+    strftime(timestamp, 20, "%d/%m/%Y %H:%M:%S", localtime(&now));
+    fprintf(fptr, "[Driver] [%s]: [%s] [%s]\n", timestamp, command, info);
+    fclose(fptr);
+}
 
 int main(int argc, char *argv[]) {
     int sockfd;
@@ -34,6 +50,8 @@ int main(int argc, char *argv[]) {
     sprintf(buffer, "%s %s", command, info);
     sendto(sockfd, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
+    log_message(command, info); // log command and info
+
     printf("[Driver]: [%s] [%s]\n", command, info);
 
     int n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 0, NULL, NULL);
@@ -43,4 +61,3 @@ int main(int argc, char *argv[]) {
     close(sockfd);
     return 0;
 }
-
